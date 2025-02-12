819a24b081c6f5dc550d824aa1685a9dc7daca7e
m1xttt <m1xtik.official@gmail.com>
2025-02-12 18:15:44 +0300

    Add files via upload

---
+++ static/scripts/reservation.js
@@ -0,0 +1,10 @@
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+    $("#term").mask("99/99");
+});
+jQuery(document).ready(function($) {
+    $("#number").mask("9999999999999999");
+});
+jQuery(document).ready(function($) {
+    $("#CVV").mask("999");
+});
